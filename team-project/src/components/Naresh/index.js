398be5bc8bec2eb686783743a4459fc5f0eb41f1
venkatgit config --global user.email venkata.krishna@concertidc.com <venkata.krishna@concertidc.com>
2023-02-04 12:00:25 +0530

    naresh code update

--- team-project/src/components/Naresh/index.js
+++ team-project/src/components/Naresh/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './index.css';
 
 export default class Naresh extends Component {
   render() {
     return (
       <div>
-        <h1>Naresh</h1>
+        <h1 className='head'>i am Naresh and i learn react js</h1>
       </div>
     )
   }
